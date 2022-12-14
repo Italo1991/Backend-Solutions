9f9392ccd5ef0849665d65aac8878a156d479f0f
Italo Duarte <italo.biguzzi@gmail.com>
2022-12-14 07:16:07 -0300

    Unit testing

---
+++ Italo.Customer.Tests/Test.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Italo.Customer.Tests
+{
+    public class Test
+    {
+        public Test()
+        {
+
+        }
+
+        [Fact]
+        public void Validate()
+        {
+            var number = 1;
+            number.Should().Be(1);
+        }
+    }
+}
