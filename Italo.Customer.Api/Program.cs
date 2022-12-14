95f1f5a377b4a348a0b9e940dc36d236eec22e91
Italo Duarte <italo.biguzzi@gmail.com>
2022-12-14 17:23:39 -0300

    Swagger

--- Italo.Customer.Api/Program.cs
+++ Italo.Customer.Api/Program.cs
@@ -17,11 +17,8 @@ builder.AddInfrastructureExtension();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
